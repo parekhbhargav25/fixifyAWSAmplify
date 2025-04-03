9103018a1eb96fab958d69d230bb1cd634c67ade
Bhargav parekh <39110031+parekhbhargav25@users.noreply.github.com>
2025-04-03 14:39:50 -0400

    Update reviewApi.js

--- source_code/frontend/src/api/reviewApi.js
+++ source_code/frontend/src/api/reviewApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL = "http://localhost:3000"
+const URL = "https://fixifyawsamplify-production.up.railway.app/"
 
 // export async function getReviewsByUserId(id) {
 //     const response = await axios.get(`${URL}/reviews/${id}`)
